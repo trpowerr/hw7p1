2a1d630bd5ad703ee44368f09e2fed4094ec98d3
trpowerr <48128809+trpowerr@users.noreply.github.com>
2019-12-09 18:34:49 +0300

    Update constructor.js

--- src/js/constructor.js
+++ src/js/constructor.js
@@ -1,11 +1,14 @@
 function createPerson (name, type) {
-  if ( name.length > 2 && name.length < 10 && giveTypePerson(type) != undefined) {
+  if ( name.length > 2 && name.length < 10 && findBy('type', type) != undefined) {
     const person = {
       name,
-      type,
     };
+    
+    const arr = findBy('type', type);
 
-    return person;
+    const obj = Object.assign(person,arr);
+
+    return obj;
 
   } else {
     throw new Error('Wait, my boy, you are crazy?')
@@ -13,28 +16,19 @@ function createPerson (name, type) {
   
 }
 
-function giveTypePerson (type) {
-  const typePerson = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
-  for (let value of typePerson) {
-    if (value == type) {
-      return type;
-    }
-  }
+const results = [
+  {type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25},
+  {type: 'Swordsman', health: 100, level: 1, attack: 40, defence: 10},
+  {type: 'Magician', health: 100, level: 1, attack: 10, defence: 40},
+  {type: 'Daemon', health: 100, level: 1, attack: 25, defence: 25},
+  {type: 'Undead', health: 100, level: 1, attack: 40, defence: 10},
+  {type: 'Zombie', health: 100, level: 1, attack: 10, defence: 40},,
+]
+
+function findBy (key,value) {
+  return results.find( ({ [key]: val }) => val === value);
 }
 
-function unitDamageDefence (type) {
-  const obj = [
-    {type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25},
-    {type: 'Swordsman', health: 100, level: 1, attack: 40, defence: 10},
-    {type: 'Magician', health: 100, level: 1, attack: 10, defence: 40},
-    {type: 'Daemon', health: 100, level: 1, attack: 25, defence: 25},
-    {type: 'Undead', health: 100, level: 1, attack: 40, defence: 10},
-    {type: 'Zombie', health: 100, level: 1, attack: 10, defence: 40},
-  ];
-
-  
-}
-
-const ourHero = createPerson('MIsha', 'Daemon')
+const ourHero = createPerson('Masha', 'Undead')
 
 console.log(ourHero);
